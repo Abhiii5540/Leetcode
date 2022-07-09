7e92178f4bf239d37ad57681e993ccd4edbeb4b9
ABHISHEK KUMAR <70334768+Abhiii5540@users.noreply.github.com>
2022-07-09 23:10:50 +0530

    Update 46-permutations.java

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -7,7 +7,8 @@ class Solution {
         permutationNum(a, n, l, ds, visit);
         return l;
     }
-    
+        // taking a extra space for a list and array for visiting n not visiting
+
     private void permutationNum(int a[], int n, List<List<Integer>> l, List<Integer> ds,
             boolean visit[]) {
         if (ds.size() == n) {
@@ -27,3 +28,30 @@ class Solution {
         }
     }
 }
+
+
+
+ // nowdoing it without taking a ds list n a vistied bollean array
+    // can be done by swapping with each element
+    static List<List<Integer>> permutationNum2(int a[], int n, List<List<Integer>> l, int index) {
+        if (index == n) {
+            List<Integer> ds = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                ds.add(a[j]);
+            }
+            l.add(new ArrayList<>(ds));
+            return l;
+        }
+        for (int j = index; j < n; j++) {
+            swap(a, j, index);
+            permutationNum2(a, n, l, index + 1);
+            swap(a, j, index);
+        }
+        return l;
+    }
+
+    private static void swap(int a[], int index, int j) {
+        int t = a[index];
+        a[index] = a[j];
+        a[j] = t;
+    }
