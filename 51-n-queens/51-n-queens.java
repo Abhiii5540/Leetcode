12bcf56e8e4160f3c90a1483c1230ee8846af27e
ABHISHEK KUMAR <70334768+Abhiii5540@users.noreply.github.com>
2022-07-10 16:27:02 +0530

    Time: 4 ms (77.43%), Space: 44.5 MB (80.70%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,5 +1,7 @@
 class Solution {
-     public static List<List<String>> solveNQueens(int n) {
+     
+    /*
+    public static List<List<String>> solveNQueens(int n) {
         char[][] board = new char[n][n];
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
@@ -57,6 +59,69 @@ class Solution {
                 board[row][col] = 'Q';
                 solve(col + 1, board, res);
                 board[row][col] = '.';
+            }
+        }
+    }
+
+    static List<String> construct(char[][] board) {
+        List<String> res = new LinkedList<String>();
+        for (int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        return res;
+    }
+}
+*/
+
+// Time Complexity: Exponential in nature, since we are trying out all ways. 
+// To be precise it goes as O (N! * N) nearly.
+
+// Space Complexity: O(N^2)
+
+
+
+
+// Solution 2: 
+
+// Intuition: This is the optimization of the issafe function. In the previous issafe function,
+//  we need o(N) for a row, o(N) for the column, and o(N) for diagonal. 
+//  Here, we will use hashing to maintain a list to check whether that position can be the right
+//  one or not.
+
+
+    public static List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = '.';
+        List<List<String>> res = new ArrayList<List<String>>();
+        int leftRow[] = new int[n];
+        int upperDiagonal[] = new int[2 * n - 1];
+        int lowerDiagonal[] = new int[2 * n - 1];
+        solve(0, board, res, leftRow, lowerDiagonal, upperDiagonal);
+        return res;
+    }
+
+    static void solve(int col, char[][] board, List<List<String>> res, int leftRow[], int lowerDiagonal[],
+            int upperDiagonal[]) {
+        if (col == board.length) {
+            res.add(construct(board));
+            return;
+        }
+
+        for (int row = 0; row < board.length; row++) {
+            if (leftRow[row] == 0 && lowerDiagonal[row + col] == 0
+                    && upperDiagonal[board.length - 1 + col - row] == 0) {
+                board[row][col] = 'Q';
+                leftRow[row] = 1;
+                lowerDiagonal[row + col] = 1;
+                upperDiagonal[board.length - 1 + col - row] = 1;
+                solve(col + 1, board, res, leftRow, lowerDiagonal, upperDiagonal);
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiagonal[row + col] = 0;
+                upperDiagonal[board.length - 1 + col - row] = 0;
             }
         }
     }
