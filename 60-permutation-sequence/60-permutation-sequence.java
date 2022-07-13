a1bf1faa3e8b697a7b770ad2cf59ead74dd6bdfa
ABHISHEK KUMAR <70334768+Abhiii5540@users.noreply.github.com>
2022-07-14 02:25:53 +0530

    Time: 2 ms (92.35%), Space: 40.3 MB (86.77%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.java
+++ 60-permutation-sequence/60-permutation-sequence.java
@@ -1,4 +1,60 @@
 class Solution {
+    
+    // APPROACH 1- 
+        
+     /*
+     * The extreme naive solution is to generate all the possible permutations of
+     * the given sequence. This is achieved using recursion and every permutation
+     * generated is stored in some other data structure (here we have used a
+     * vector). Finally, we sort the data structure in which we have stored all the
+     * sequences and return the Kth sequence from it.
+     */
+
+    // static void swap(char s[], int i, int j) {
+    // char ch = s[i];
+    // s[i] = s[j];
+    // s[j] = ch;
+    // }
+
+    // static void solve(char s[], int index, ArrayList<String> res) {
+    // if (index == s.length) {
+    // String str = new String(s);
+
+    // res.add(str);
+    // return;
+    // }
+    // for (int i = index; i < s.length; i++) {
+    // swap(s, i, index);
+    // solve(s, index + 1, res);
+    // swap(s, i, index);
+    // }
+    // }
+
+    // static String getPermutation(int n, int k) {
+    // String s = "";
+    // ArrayList<String> res = new ArrayList<>();
+    // for (int i = 1; i <= n; i++) {
+    // s += i;
+    // }
+    // solve(s.toCharArray(), 0, res);
+    // Collections.sort(res);
+
+    // return res.get(k);
+
+    // }
+
+    
+    
+        // APPROACH 2- 
+    /*
+     * Since this is a permutation we can assume that there are four positions that
+     * need to be filled using the four numbers of the sequence. First, we need to
+     * decide which number is to be placed at the first index. Once the number at
+     * the first index is decided we have three more positions and three more
+     * numbers. Now the problem is shorter. We can repeat the technique that was
+     * used previously until all the positions are filled.
+     */
+
     public String getPermutation(int n, int k) {
         List<Integer> l=new ArrayList<>();
         int f=1;
