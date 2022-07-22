9cc835409cc71c3fd136586f8c8b027b5b9129b7
ABHISHEK KUMAR <70334768+Abhiii5540@users.noreply.github.com>
2022-07-23 03:05:07 +0530

    Time: 1 ms (94.70%), Space: 42.9 MB (31.16%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -13,22 +13,55 @@
  *     }
  * }
  */
+
 class Solution {
+    
+        // DFS solution
+//     public List<Integer> rightSideView(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         rightSideViewHelper(root , 0 , ans);
+//         return ans;
+//     }
+    
+//    public void rightSideViewHelper(TreeNode root , int level , List<Integer> ans){
+//         if(root== null){
+//             return;
+//         }
+//         if(ans.size()==level){
+//             ans.add(root.val);
+//         }
+//         rightSideViewHelper(root.right , level+1 , ans);
+//         rightSideViewHelper(root.left , level+1 , ans);   
+//     }
+    
+    
+    // BFS solution 
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
         rightSideViewHelper(root , 0 , ans);
         return ans;
     }
     
-   public void rightSideViewHelper(TreeNode root , int level , List<Integer> ans){
-        if(root== null){
+    public void rightSideViewHelper(TreeNode root , int level , List<Integer> ans){
+        if (root == null) {
             return;
         }
-        if(ans.size()==level){
-            ans.add(root.val);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int count = queue.size();
+            for (int i = 0; i < count; i++) {
+                TreeNode node = queue.poll();
+                if (i==0) {
+                    ans.add(node.val);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+            }
         }
-        rightSideViewHelper(root.right , level+1 , ans);
-        rightSideViewHelper(root.left , level+1 , ans);
-        
     }
 }
