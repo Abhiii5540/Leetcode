40446a1fdcb8ebe2dca9311410f2031baf7584d9
ABHISHEK KUMAR <70334768+Abhiii5540@users.noreply.github.com>
2022-08-09 00:53:39 +0530

    Time: 1 ms (80.19%), Space: 43.5 MB (16.87%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -10,21 +10,22 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode temp=new ListNode(-1);
+        /*
+        ListNode temp=new ListNode(-1);//-1 means null node
         ListNode curr=temp;
-        while(l1!=null && l2!=null){
-            if(l1.val<l2.val){
-                curr.next=l1;
-                curr=curr.next;
-                l1=l1.next;
+        while(l1!=null && l2!=null){//till both ll is empty
+            if(l1.val<l2.val){// if element in 1st ll is smaller
+                curr.next=l1;//add that element to new ll
+                curr=curr.next;//shift curr pointer
+                l1=l1.next;//shift l1 pointer
             }
-            else{
+            else{// if element in 2nd ll is smaller
                 curr.next=l2;
                 curr=curr.next;
                 l2=l2.next;
             }
         }
-        
+        //if either of them is unfinished
         if(l1!=null){
             curr.next=l1;
         }
@@ -33,5 +34,29 @@ class Solution {
             curr.next=l2;
         }
         return temp.next;
+        
+        */
+        
+        if(l1==null) return l2;
+        if(l2==null) return l1;
+        if(l1.val>l2.val){//swap l1 and l2
+            ListNode temp=l1;
+            l1=l2;
+            l2=temp;
+        }
+        ListNode res=l1;
+        while(l1!=null && l2!=null){
+            ListNode dummy=null;
+            while(l1!=null && l1.val<=l2.val){
+                dummy=l1;
+                l1=l1.next;
+            }
+            dummy.next=l2;
+            
+            ListNode temp=l1;
+            l1=l2;
+            l2=temp;
+        }
+    return res;
     }
-    }
+}
