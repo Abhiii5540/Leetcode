24b86a1c18b1df75e3f096da79bbfb2ab8f43d52
ABHISHEK KUMAR <70334768+Abhiii5540@users.noreply.github.com>
2022-08-21 01:59:53 +0530

    Time: 2 ms (70.99%), Space: 44 MB (34.33%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -3,25 +3,25 @@ class Solution {
         int closing=0;
         int opening=0;
         List<String> ans=new ArrayList<>();
-        generateParenthesisLogic(n ,opening , closing , ans , "");
+        generateParenthesisHelper(n ,opening , closing , ans , "");
         return ans;
     }
     
-    private static void  generateParenthesisLogic (int n ,int opening , int closing ,  List<String> ans , String brackets){
+    private static void  generateParenthesisHelper (int n ,int opening , int closing ,  List<String> ans , String brackets){
         // Termination case
         // as for n , N*2 brackets can be present as 3 open n 3 closed
         if(brackets.length()==n*2){
             ans.add(brackets);
             return;
         }
-        // Number of opening brackets should be less than n
+        // Number of opening brackets should be less than n , so that we can add the opening in the ans
         if(opening <n){
-            generateParenthesisLogic(n ,opening+1 , closing , ans , brackets+"(");
+            generateParenthesisHelper(n ,opening+1 , closing , ans , brackets+"(");
   
         }
-        // Number of closing brackets should be less than opening
+        // Number of closing brackets should be less than opening foradding closing brackets in the ans.
         if(closing<opening){
-             generateParenthesisLogic(n ,opening , closing+1 , ans , brackets+")");
+             generateParenthesisHelper(n ,opening , closing+1 , ans , brackets+")");
 
 
         }
